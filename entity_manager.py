4e5dfc8272e053a4e2dd9f3b30bf654ba3c545e3
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-11-19 00:16:44 +0000

    uploaded wrong file for entity manager

--- entity_manager.py
+++ entity_manager.py
@@ -1,36 +1,21 @@
-import pygame
-import math
-from anim import Animator
-import actor
+class entityManager:
+    def __init__(self):
+        self.entities = dict()
+        self.entities["Player"] = []
+        self.entities["enemy"] = []
+        self.entities["bullet"] = []
 
-speed = 50
-
-class projectile: 
-    def __init__(self,color,x,y,width,height):
-        self.anim = Animator(color,x,y,width,height)
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect(
-            x,y,width,height
-        )
-        x_mouse,y_mouse = pygame.mouse.get_pos()
-        x_center,y_center = self.rect.center
-        self.angle = math.atan2(x_mouse-x_center,y_mouse-y_center)
-        self.kinem = actor.Kinematics(self)
-        self.kinem.vel_x = -10*math.cos(self.angle+(3.14/2))
-        self.kinem.vel_y = 10*math.sin(self.angle+(3.14/2))
-        self.x = x
-        self.y = y
-
-    def update(self):
-        self.x = self.x + self.kinem.vel_x
-        self.y = self.y + self.kinem.vel_y
-        self.rect.x = int(self.x)
-        self.rect.y = int(self.y)
+    def addEntity(self,entity,entityType):
+        self.entities[entityType].append(entity)
     
-    def render(self,screen):
-        img, rect = self.anim.rotate(90+self.angle*180/3.14)
-        screen.blit(img,self.rect)
-    
-    def destroy(self):
-        pass
+    def removeEntity(self,entity,entityType):
+        self.entities[entityType].remove(entity)
+
+    def updateAll(self):
+        for entity in self.entities["Player"]:
+            entity.update()
+
+    def renderAll(self,screen):
+        for entity in self.entities["Player"]:
+            entity.render(screen)
+            
