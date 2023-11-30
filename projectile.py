ef9de5ed1aae4e3c0b9cdd73eeb1aaa5fc24ad70
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-11-30 04:02:36 +0000

    updated projectile to have a base image

--- projectile.py
+++ projectile.py
@@ -4,23 +4,32 @@ from anim import Animator
 import actor
 
 speed = 50
+class projectileImage:
+    def __init__(self):
+        self.image = pygame.Surface((10,5))
+        self.image.fill((255,0,0))
+        self.image.set_colorkey((0,0,0))
 
-class projectile: 
-    def __init__(self,color,x,y,width,height):
-        self.anim = Animator(color,x,y,width,height)
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect(
-            x,y,width,height
-        )
+    def render(self,screen,rect,angle):
+        angle = (angle*180/3.14)+90
+        screen.blit(pygame.transform.rotate(self.image,angle),rect)
+    
+
+baseProjectile = projectileImage()
+
+class projectile(pygame.sprite.Sprite): 
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = baseProjectile
+        self.anim = Animator(baseProjectile.image,x+25,y+25)
+        self.rect = self.image.image.get_rect()
         x_mouse,y_mouse = pygame.mouse.get_pos()
-        x_center,y_center = self.rect.center
-        self.angle = math.atan2(x_mouse-x_center,y_mouse-y_center)
+        self.angle = math.atan2(x_mouse-x,y_mouse-y)
         self.kinem = actor.Kinematics(self)
-        self.kinem.vel_x = -10*math.cos(self.angle+(3.14/2))
-        self.kinem.vel_y = 10*math.sin(self.angle+(3.14/2))
-        self.x = x
-        self.y = y
+        self.kinem.vel_x = -speed*math.cos(self.angle+(3.14/2))
+        self.kinem.vel_y = speed*math.sin(self.angle+(3.14/2))
+        self.x = x+25
+        self.y = y+25
 
     def update(self):
         self.x = self.x + self.kinem.vel_x
@@ -29,8 +38,7 @@ class projectile:
         self.rect.y = int(self.y)
     
     def render(self,screen):
-        img, rect = self.anim.rotate(90+self.angle*180/3.14)
-        screen.blit(img,self.rect)
+        self.image.render(screen,self.rect,self.angle)
     
     def destroy(self):
-        pass
+        self.kill()
