6d6d42a6d40f346ab279a9b4915811e08eb49e0e
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-11-30 18:42:27 +0000

    I think I changed to movement speed

--- state.py
+++ state.py
@@ -20,7 +20,6 @@ class moving(PlayerState):
         self.a = True
         self.s = True
         self.d = True
-        self.esc = False
 
     def processInput(self, pressed):
         xspeed = 0
@@ -28,35 +27,28 @@ class moving(PlayerState):
         if pressed[keys.K_SPACE]:
             self.parent.gun()
         if pressed[keys.K_w] and self.w == True:
-            yspeed -=10
+            yspeed -=5
             self.w = False
         else:
-            yspeed +=10 
+            yspeed +=5 
             self.w = True
         if pressed[keys.K_a] and self.a == True:
-            xspeed -=10
+            xspeed -=5
             self.a = False
         else: 
-            xspeed +=10
+            xspeed +=5
             self.a = True
         if pressed[keys.K_s] and self.s == True:
-            yspeed +=10
+            yspeed +=5
             self.s = False
         else: 
-            yspeed -=10
+            yspeed -=5
             self.s = True
         if pressed[keys.K_d] and self.d == True:
-            xspeed +=10
+            xspeed +=5
             self.d = False
         else: 
-            xspeed -=10
+            xspeed -=5
             self.d = True
-        if pressed[keys.K_ESCAPE] and not self.esc:
-            self.esc = True
-            MenuState.ScreenOverlay()
-        if not pressed[keys.K_ESCAPE]:
-            self.esc = False
-
         self.parent.kinem.vel_x = xspeed
         self.parent.kinem.vel_y = yspeed
-        
