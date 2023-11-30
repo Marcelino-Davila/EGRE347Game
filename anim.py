3bbf85803570474de56e8709f811fb4edba262a1
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-11-30 04:04:36 +0000

    cut some stuff to make the base images work

--- anim.py
+++ anim.py
@@ -1,18 +1,13 @@
 import pygame
 
 class Animator:
-    def __init__(self,color,x,y,width,height):
-        self.color = color
-        self.image = pygame.Surface((width, height))
-        self.image.fill(color)
-        self.image.set_colorkey((0,0,0))
-        self.rect = self.image.get_rect()
+    def __init__(self,image,x,y,scale=0):
+        self.surf = pygame.transform.scale_by(image,scale)
+        self.rect = self.surf.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.rect.center = (250,250)
+        
 
     def rotate(self,angle):
-        img1 = pygame.transform.rotate(self.image,angle)
-        rect1 = img1.get_rect()
-        rect1.center = self.rect.center
-        return img1,rect1
+        img = pygame.transform.rotate(self.surf,angle)
+        return img
