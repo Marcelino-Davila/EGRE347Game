e8ddcfaf12f7a07d14528f825030bd63feb38ce8
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-12-04 21:47:56 +0000

    adds collision detection for walls bullets enemies

--- entity_manager.py
+++ entity_manager.py
@@ -9,6 +9,7 @@ class entityManager:
         self.PowerUps = pg.sprite.Group()
         self.Weapons = pg.sprite.Group()
         self.buttons = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
 
     def addEntity(self,entity,entityType):
         if entityType == "Player":
@@ -25,6 +26,8 @@ class entityManager:
             self.Weapons.add(entity)
         elif entityType == "Buttons":
             self.buttons.add(entity)
+        elif entityType == "Walls":
+            self.walls.add(entity)
     
     def removeEntity(self,entity,entityType):
         if entityType == "Player":
@@ -41,6 +44,8 @@ class entityManager:
             self.Weapons.remove(entity)
         elif entityType == "Buttons":
             self.buttons.remove(entity)
+        elif entityType == "Walls":
+            self.walls.remove(entity)
 
     def updateAll(self,pause):
         if not pause:
@@ -57,6 +62,8 @@ class entityManager:
             for entity in self.PowerUps:
                 entity.update()
             for entity in self.Weapons:
+                entity.update()
+            for entity in self.walls:
                 entity.update()
         else:
             for entity in self.buttons:
@@ -76,12 +83,34 @@ class entityManager:
                 entity.render(screen)
             for entity in self.Weapons:
                 entity.render(screen)
+            for entity in self.walls:
+                entity.render(screen)
         else:
             for entity in self.buttons:
                 entity.render(screen)
+    def checkCollisions(self):
+        for bullets in self.AllyBullets:
+            for walls in self.walls:
+                if pg.sprite.collide_rect(bullets,walls):
+                    self.removeEntity(bullets,"AllyBullets")
+        for player in self.player:
+            for walls in self.walls:
+                if pg.sprite.collide_rect(player,walls):
+                    player.kinem.revertX()
+                    player.kinem.revertY()
+        for enemies in self.enemies:
+            for bullets in self.AllyBullets:
+                if pg.sprite.collide_rect(bullets,enemies):
+                    enemyDead = enemies.collisionBullet()
+                    if enemyDead:
+                        self.removeEntity(enemies,"Enemy")
+                    self.removeEntity(bullets,"AllyBullets")
+        for player in self.player:
+            for enemies in self.enemies:
+                if pg.sprite.collide_rect(player.detectRange,enemies):
+                    enemies.alert = True
+                else:
+                    enemies.alert = False
 
     def loadLevel(self):#takes in all the data from the json file and adds every entity to the entity manager
         pass
-            
-class collisionHandler:
-    pass
