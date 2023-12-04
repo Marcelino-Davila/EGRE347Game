766b16ddea88bcead2aa6e1582ac1a94f05b1b83
Marcelino-Davila <149441206+Marcelino-Davila@users.noreply.github.com>
2023-12-04 21:56:19 +0000

    added enemies and stuff to the entity manager

--- game.py
+++ game.py
@@ -2,13 +2,14 @@ import pygame
 from entity_manager import entityManager
 import PlayerClass
 import enemies
+import actor
 from LevelManager import MapInformation
 
 MapName, MapImagePath, MapHeight, MapWidth = MapInformation()
 
 width = MapWidth
 height = MapHeight
-FPS = 240
+FPS = 120
 
 green = (0,255,0)
 black = (0,0,0)
@@ -25,9 +26,12 @@ class Game:
             pygame.QUIT,
             pygame.KEYDOWN
         ])
-        self.player = PlayerClass.Gojira(250,250)
-        self.enemy = enemies.enemies(200,200)
+        self.player = PlayerClass.soldier(750,750)
+        self.wall = actor.coliders(100,100,10,200)
+        self.enemy = enemies.enemies(200,200,self.player)
         self.entityManager.addEntity(self.player,"Player")
+        self.entityManager.addEntity(self.enemy,"Enemy")
+        self.entityManager.addEntity(self.wall,"Walls")
         self.MapImage = pygame.image.load(MapImagePath)
         
     def loop(self):
@@ -37,10 +41,13 @@ class Game:
                     return
                 
             self.screen.blit(self.MapImage, (0, 0))
-            
-            self.entityManager.renderAll(self.screen)
-            self.entityManager.updateAll()
+            self.entityManager.renderAll(self.screen,False)#need to know if paused
+            self.entityManager.updateAll(False)
+            self.entityManager.checkCollisions()
             self.player.processInput(pygame.key.get_pressed())
+            bullet = self.player.processMouse(pygame.mouse.get_pressed())
+            if bullet:
+                self.entityManager.addEntity(bullet,"AllyBullets")
             pygame.display.flip()
             self.clock.tick(FPS)
 
